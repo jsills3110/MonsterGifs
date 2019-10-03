c4091c1dc164d6445a9cff0e83062feb6e4f4769
jsills3110 <jsills3110@gmail.com>
2019-10-03 13:53:08 -0700

    Clicking on the favorite button for a gif now changes the icon from an empty star to a solid star. It also adds the id of the gif to an array. If the gif is unfavorited, then it will remove the id of the gif from the array.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,18 +11,27 @@ var warnings = $("#warnings");
 // Create a new button for each entry in the monsters and searches arrays. Add the
 // button to the DOM.
 function renderButtons() {
-    warnings.empty();
-    buttonHolder.empty();
+    warnings.empty(); // Clear the warnings div
+    buttonHolder.empty(); // Clear the buttonHolder div
 
+    // For each entry in the default monsters list...
     for (var i = 0; i < monsters.length; i++) {
+        
+        // Create a button with the details of that entry
         var button = $("<button>");
         button.attr("data-search", monsters[i]);
         button.text(monsters[i]);
         button.addClass("btn btn-dark mr-2 mb-2 search-for-me");
         buttonHolder.append(button);
     }
+
+    // As long as the searches array is not empty...
     if (searches.length > 0) {
+        
+        // For each entry in the searches list...
         for (var i = 0; i < searches.length; i++) {
+            
+            // Create a button with the details of that entry
             var button = $("<button>");
             button.attr("data-search", searches[i]);
             button.text(searches[i]);
@@ -35,8 +44,8 @@ function renderButtons() {
 // When a button is clicked, search for the content of the button via the GIPHY API,
 // and add the gifs to the screen.
 $(document).on("click", ".search-for-me", function () {
-    warnings.empty();
-    gifHolder.empty();
+    warnings.empty(); // Clear the warnings div
+    gifHolder.empty(); // Clear the gifHolder div
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).attr("data-search") + "&api_key=" + giphyAPI + "&limit=10";
 
@@ -49,12 +58,14 @@ $(document).on("click", ".search-for-me", function () {
 
         // For each gif in the response, create an <img> for it and add it to the DOM.
         for (var i = 0; i < searchResults.length; i++) {
-            console.log(searchResults[i]);
+            // console.log(searchResults[i]);
             
+            // Create a Bootstrap Card.
             var gifCard = $("<div>");
             gifCard.addClass("card d-inline-block mr-3 mb-3");
             gifCard.attr("style", "background-color: rgba(36, 40, 45, 0.8);");
             
+            // Create a new image and set it to the gif.
             var gif = $("<img>");
             gif.attr("src", searchResults[i].images.fixed_height_still.url);
             gif.attr("data-still", searchResults[i].images.fixed_height_still.url);
@@ -63,15 +74,21 @@ $(document).on("click", ".search-for-me", function () {
             gif.addClass("gif card-img-top mr-2 ml-2 mt-2");
             gif.attr("style", "width: auto;");
             
+            // Create the Bootstrap Card body.
             var gifCardBody = $("<div>");
             gifCardBody.addClass("card-body");
 
+            // Create the "Favorite" button.
             var favButton = $("<button>");
             favButton.html("<i class='far fa-star' style='color: rgb(253, 255, 153)'></i>");
-            favButton.addClass("btn btn-secondary add-favorite d-inline-block mr-2");
+            favButton.addClass("btn btn-secondary d-inline-block mr-2 add-favorite");
+            favButton.attr("data-gif-id", searchResults[i].id);
+            favButton.attr("data-not-favorite", "<i class='far fa-star' style='color: rgb(253, 255, 153)'></i>")
+            favButton.attr("data-favorite", "<i class='fas fa-star' style='color: rgb(253, 255, 153)'></i>")
+            favButton.attr("data-state", "not-favorite");
 
+            // Append the card elements to the card, then append the card to gifHolder.
             gifCard.append(gif);
-            // gifCardBody.append("<h5 class='card-title'>Title: " + searchResults[i].title + "</h5>");
             gifCardBody.append(favButton);
             gifCardBody.append("<p class='card-text text-light d-inline-block'>Rating: " + searchResults[i].rating + "</p>")
             gifCard.append(gifCardBody)
@@ -82,13 +99,20 @@ $(document).on("click", ".search-for-me", function () {
 
 // When a gif is clicked, switch it from still to animate, or animate to still.
 $(document).on("click", ".gif", function () {
-    warnings.empty();
+    warnings.empty(); // Clear the warnings div
     var clickedGif = $(this);
 
+    // If the current state of the gif is still...
     if (clickedGif.attr("data-state") === "still") {
+        
+        // Set the src to the animate url and set the state to animate
         clickedGif.attr("src", clickedGif.attr("data-animate"));
         clickedGif.attr("data-state", "animate");
+    
+    // If the current state of the gif is animate...
     } else {
+        
+        // Set the src to the still url and set the state to still
         clickedGif.attr("src", clickedGif.attr("data-still"));
         clickedGif.attr("data-state", "still");
     }
@@ -96,26 +120,50 @@ $(document).on("click", ".gif", function () {
 
 // When "Add GIF Button" is clicked, add the text box content as a button to the screen.
 $("#add-a-gif").on("click", function (event) {
-    event.preventDefault();
+    event.preventDefault(); // Prevent the page from refreshing upon submit
+    warnings.empty(); // Clear the warnings div
 
-    warnings.empty();
-
+    // Grab the user's input from the textbox
     var input = $("#customSearch").val().trim().toLowerCase();
 
-    console.log(input.toLowerCase());
+    // console.log(input.toLowerCase());
 
+    // If the input is blank...
     if (input === "") {
         warnings.append("<h3>You cannot create a button with nothing! Try again.</h3>");
+    
+    // If the user has NOT already created a button for this search...
     } else if (monsters.indexOf(input) === -1 && searches.indexOf(input) === -1) {
-        searches.push(input);
-
-        $("#customSearch").val("");
-
-        renderButtons();
+        searches.push(input); // Add the search to our searches array
+        $("#customSearch").val(""); // Reset the textbox to blank
+        renderButtons(); // Render the new button list
+    
+    // If the user already created a button for this search...
     } else {
         warnings.append("<h3>There is already a button for that; try again.</h3>");
-        $("#customSearch").val("");
+        $("#customSearch").val(""); // Reset the textbox to blank
     }
+});
+
+$(document).on("click", ".add-favorite", function () {
+    var clickedFave = $(this);
+    
+    if (clickedFave.attr("data-state") === "not-favorite") {
+        clickedFave.html(clickedFave.attr("data-favorite"));
+        clickedFave.attr("data-state", "favorite");
+        if (favorites.indexOf(clickedFave.attr("data-gif-id")) === -1) {
+            favorites.push(clickedFave.attr("data-gif-id"));
+        }
+    } else {
+        clickedFave.html(clickedFave.attr("data-not-favorite"));
+        clickedFave.attr("data-state", "not-favorite");
+        if (favorites.indexOf(clickedFave.attr("data-gif-id")) !== -1) {
+            favorites.splice(favorites.indexOf(clickedFave.attr("data-gif-id")), 1);
+        }
+    }
+
+    console.log(favorites);
+
 });
 
 renderButtons();
