5d3d3b5ee49d0af4e8f17b13c52e7abe58b2e4d9
jsills3110 <jsills3110@gmail.com>
2019-10-03 14:20:03 -0700

    Added a Show Favorites button which displays the user's favorited gifs. Favorites persist even when the user selects a different search.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,9 +14,15 @@ function renderButtons() {
     warnings.empty(); // Clear the warnings div
     buttonHolder.empty(); // Clear the buttonHolder div
 
+    var favoriteButton = $("<button>");
+    favoriteButton.html("<i class='fas fa-star' style='color: rgb(253, 255, 153)'></i> Show Favorites");
+    favoriteButton.addClass("btn btn-dark mr-2 mb-2");
+    favoriteButton.attr("id", "show-favorites");
+    buttonHolder.append(favoriteButton);
+
     // For each entry in the default monsters list...
     for (var i = 0; i < monsters.length; i++) {
-        
+
         // Create a button with the details of that entry
         var button = $("<button>");
         button.attr("data-search", monsters[i]);
@@ -27,10 +33,10 @@ function renderButtons() {
 
     // As long as the searches array is not empty...
     if (searches.length > 0) {
-        
+
         // For each entry in the searches list...
         for (var i = 0; i < searches.length; i++) {
-            
+
             // Create a button with the details of that entry
             var button = $("<button>");
             button.attr("data-search", searches[i]);
@@ -59,12 +65,12 @@ $(document).on("click", ".search-for-me", function () {
         // For each gif in the response, create an <img> for it and add it to the DOM.
         for (var i = 0; i < searchResults.length; i++) {
             // console.log(searchResults[i]);
-            
+
             // Create a Bootstrap Card.
             var gifCard = $("<div>");
             gifCard.addClass("card d-inline-block mr-3 mb-3");
             gifCard.attr("style", "background-color: rgba(36, 40, 45, 0.8);");
-            
+
             // Create a new image and set it to the gif.
             var gif = $("<img>");
             gif.attr("src", searchResults[i].images.fixed_height_still.url);
@@ -73,19 +79,25 @@ $(document).on("click", ".search-for-me", function () {
             gif.attr("data-state", "still");
             gif.addClass("gif card-img-top mr-2 ml-2 mt-2");
             gif.attr("style", "width: auto;");
-            
+
             // Create the Bootstrap Card body.
             var gifCardBody = $("<div>");
             gifCardBody.addClass("card-body");
 
             // Create the "Favorite" button.
             var favButton = $("<button>");
-            favButton.html("<i class='far fa-star' style='color: rgb(253, 255, 153)'></i>");
             favButton.addClass("btn btn-secondary d-inline-block mr-2 add-favorite");
-            favButton.attr("data-gif-id", searchResults[i].id);
             favButton.attr("data-not-favorite", "<i class='far fa-star' style='color: rgb(253, 255, 153)'></i>")
             favButton.attr("data-favorite", "<i class='fas fa-star' style='color: rgb(253, 255, 153)'></i>")
-            favButton.attr("data-state", "not-favorite");
+            favButton.attr("data-gif-id", searchResults[i].id);
+
+            if (favorites.indexOf(searchResults[i].id) !== -1) {
+                favButton.html("<i class='fas fa-star' style='color: rgb(253, 255, 153)'></i>");
+                favButton.attr("data-state", "favorite");
+            } else {
+                favButton.html("<i class='far fa-star' style='color: rgb(253, 255, 153)'></i>");
+                favButton.attr("data-state", "not-favorite");
+            }
 
             // Append the card elements to the card, then append the card to gifHolder.
             gifCard.append(gif);
@@ -104,14 +116,14 @@ $(document).on("click", ".gif", function () {
 
     // If the current state of the gif is still...
     if (clickedGif.attr("data-state") === "still") {
-        
+
         // Set the src to the animate url and set the state to animate
         clickedGif.attr("src", clickedGif.attr("data-animate"));
         clickedGif.attr("data-state", "animate");
-    
-    // If the current state of the gif is animate...
+
+        // If the current state of the gif is animate...
     } else {
-        
+
         // Set the src to the still url and set the state to still
         clickedGif.attr("src", clickedGif.attr("data-still"));
         clickedGif.attr("data-state", "still");
@@ -131,39 +143,112 @@ $("#add-a-gif").on("click", function (event) {
     // If the input is blank...
     if (input === "") {
         warnings.append("<h3>You cannot create a button with nothing! Try again.</h3>");
-    
-    // If the user has NOT already created a button for this search...
+
+        // If the user has NOT already created a button for this search...
     } else if (monsters.indexOf(input) === -1 && searches.indexOf(input) === -1) {
         searches.push(input); // Add the search to our searches array
         $("#customSearch").val(""); // Reset the textbox to blank
         renderButtons(); // Render the new button list
-    
-    // If the user already created a button for this search...
+
+        // If the user already created a button for this search...
     } else {
         warnings.append("<h3>There is already a button for that; try again.</h3>");
         $("#customSearch").val(""); // Reset the textbox to blank
     }
 });
 
+// When the Star button is clicked on a gif, then the gif will be added to a favorites
+// list.
 $(document).on("click", ".add-favorite", function () {
     var clickedFave = $(this);
-    
+
+    // If the current state of the favorite button is not-favorite...
     if (clickedFave.attr("data-state") === "not-favorite") {
+
+        // Change the star from an outline to a solid star.
         clickedFave.html(clickedFave.attr("data-favorite"));
         clickedFave.attr("data-state", "favorite");
+
+        // If the gif id is not already in the favorites list...
         if (favorites.indexOf(clickedFave.attr("data-gif-id")) === -1) {
             favorites.push(clickedFave.attr("data-gif-id"));
         }
+
+        // If the current state of the favorite button is favorite...    
     } else {
+
+        // Change the star from a solid star to an outline.
         clickedFave.html(clickedFave.attr("data-not-favorite"));
         clickedFave.attr("data-state", "not-favorite");
+
+        // If the gif id is in the favorites list...
         if (favorites.indexOf(clickedFave.attr("data-gif-id")) !== -1) {
             favorites.splice(favorites.indexOf(clickedFave.attr("data-gif-id")), 1);
         }
     }
 
     console.log(favorites);
+});
 
+$(document).on("click", "#show-favorites", function () {
+    warnings.empty(); // Clear the warnings div
+    gifHolder.empty(); // Clear the gifHolder div
+
+    console.log(favorites.length);
+
+    if (favorites.length <= 0) {
+        warnings.append("<h3>You don't have any favorites yet!</h3>");
+    } else {
+        for (var i = 0; i < favorites.length; i++) {
+            var queryURL = "https://api.giphy.com/v1/gifs/" + favorites[i] + "?api_key=" + giphyAPI;
+            $.ajax({
+                url: queryURL,
+                method: 'GET'
+            }).then(function (response) {
+                var image = response.data;
+                
+                // Create a Bootstrap Card.
+                var gifCard = $("<div>");
+                gifCard.addClass("card d-inline-block mr-3 mb-3");
+                gifCard.attr("style", "background-color: rgba(36, 40, 45, 0.8);");
+
+                // Create a new image and set it to the gif.
+                var gif = $("<img>");
+                gif.attr("src", image.images.fixed_height_still.url);
+                gif.attr("data-still", image.images.fixed_height_still.url);
+                gif.attr("data-animate", image.images.fixed_height.url);
+                gif.attr("data-state", "still");
+                gif.addClass("gif card-img-top mr-2 ml-2 mt-2");
+                gif.attr("style", "width: auto;");
+
+                // Create the Bootstrap Card body.
+                var gifCardBody = $("<div>");
+                gifCardBody.addClass("card-body");
+
+                // Create the "Favorite" button.
+                var favButton = $("<button>");
+                favButton.addClass("btn btn-secondary d-inline-block mr-2 add-favorite");
+                favButton.attr("data-not-favorite", "<i class='far fa-star' style='color: rgb(253, 255, 153)'></i>")
+                favButton.attr("data-favorite", "<i class='fas fa-star' style='color: rgb(253, 255, 153)'></i>")
+                favButton.attr("data-gif-id", image.id);
+
+                if (favorites.indexOf(image.id) !== -1) {
+                    favButton.html("<i class='fas fa-star' style='color: rgb(253, 255, 153)'></i>");
+                    favButton.attr("data-state", "favorite");
+                } else {
+                    favButton.html("<i class='far fa-star' style='color: rgb(253, 255, 153)'></i>");
+                    favButton.attr("data-state", "not-favorite");
+                }
+
+                // Append the card elements to the card, then append the card to gifHolder.
+                gifCard.append(gif);
+                gifCardBody.append(favButton);
+                gifCardBody.append("<p class='card-text text-light d-inline-block'>Rating: " + image.rating + "</p>")
+                gifCard.append(gifCardBody)
+                gifHolder.append(gifCard);
+            });
+        }
+    }
 });
 
 renderButtons();
